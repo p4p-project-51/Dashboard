6a5b534ff32d4703a5fe6dbaa0dfb19ab9d6684c
Excigma <git@excigma.xyz>
2025-06-29 13:13:12 +1200

    chore: add welcome boilerplate

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import { ColorSchemeToggle } from "../components/ColorSchemeToggle/ColorSchemeToggle";
+import { Welcome } from "../components/Welcome/Welcome";
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  return (
+    <>
+      <Welcome />
+      <ColorSchemeToggle />
+    </>
+  );
 }
