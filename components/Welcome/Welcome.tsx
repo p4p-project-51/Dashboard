ce40dab8ecdbce29e44ddaa21db43ef79c88ff47
Excigma <git@excigma.xyz>
2025-09-26 11:12:07 +1200

    fix: allow wrapping

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -18,6 +18,7 @@ export function Welcome() {
         href="https://part4project.foe.auckland.ac.nz/home/project/detail/5592/"
         target="_blank"
         rel="noopener noreferrer"
+        style={{ wordWrap: "break-word" }}
       >
         <Text c="blue" ta="center" size="lg" mt="sm">
           https://part4project.foe.auckland.ac.nz/home/project/detail/5592/
